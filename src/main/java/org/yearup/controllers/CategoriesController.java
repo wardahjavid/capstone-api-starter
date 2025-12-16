519509c632e97f363aa9197b37efc513114389bf
Student <wardahjavid62@gmail.com>
2025-12-16 11:06:51 -0500

    coded @PostMapping in public Category addCategory(@RequestBody Category category) in CategoriesController.java

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -84,11 +84,16 @@ public class CategoriesController
 
     // add annotation to call this method for a POST action
     // add annotation to ensure that only an ADMIN can call this function
-
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("hasRole('ADMIN')")
     public Category addCategory(@RequestBody Category category)
     {
-        // insert the category
-        return null;
+        try{
+            return categoryDao.create(category);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "There is an error.");
+        }
     }
 
     // add annotation to call this method for a PUT (update) action - the url path must include the categoryId
