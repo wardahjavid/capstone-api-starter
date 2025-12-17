edb4b39c7ca9502d40c656a5985ef4f97270e021
Student <wardahjavid62@gmail.com>
2025-12-17 08:55:59 -0500

    coded @PostMapping in ShoppingCartController.java

--- src/main/java/org/yearup/controllers/ShoppingCartController.java
+++ src/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -2,9 +2,7 @@ package org.yearup.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import org.yearup.data.ProductDao;
 import org.yearup.data.ShoppingCartDao;
@@ -34,27 +32,63 @@ public class ShoppingCartController
     }
 
     // each method in this controller requires a Principal object as a parameter
+    @GetMapping
+    @PreAuthorize("isAuthenticated()")
     public ShoppingCart getCart(Principal principal)
     {
         try
         {
             // get the currently logged in username
+            if (principal == null) {
+                throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Not logged in.");
+            }
             String userName = principal.getName();
             // find database user by userId
             User user = userDao.getByUserName(userName);
+            if (user == null)
+                throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "User not found.");
             int userId = user.getId();
 
             // use the shoppingcartDao to get all items in the cart and return the cart
-            return null;
-        }
-        catch(Exception e)
-        {
+            ShoppingCart cart = shoppingCartDao.getByUserId(user.getId());
+            return (cart != null) ? cart : new ShoppingCart();
+        } catch (ResponseStatusException e) {
+            throw e;
+        } catch(Exception e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
         }
     }
 
     // add a POST method to add a product to the cart - the url should be
     // https://localhost:8080/cart/products/15 (15 is the productId to be added
+    @PostMapping("/products/{productId}")
+    @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("isAuthenticated()")
+    public ShoppingCart addProductToCart(@PathVariable int productId, Principal principal){
+        try {
+            if (productDao.getById(productId) == null) {
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found.");
+            }
+            int userId = getUserId(principal);
+            shoppingCartDao.addProduct(userId,productId);
+            ShoppingCart cart = shoppingCartDao.getByUserId(userId);
+            return (cart != null) ? cart : new ShoppingCart();
+        } catch (ResponseStatusException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
+        }
+    }
+    private int getUserId(Principal principal) {
+        if(principal == null)
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Not logged in.");
+        String username = principal.getName();
+        User user = userDao.getByUserName(username);
+
+        if(user == null)
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "User not found.");
+        return user.getId();
+    }
 
 
     // add a PUT method to update an existing product in the cart - the url should be
