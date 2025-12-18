8c862e4e09c74b3f382a1efe0d2427d5ddcd5d9c
Student <wardahjavid62@gmail.com>
2025-12-18 02:52:38 -0500

    coded MySqlShoppingCartDao.java (Phase 3)

--- src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
@@ -20,11 +20,19 @@ public class MySqlShoppingCartDao extends MySqlDaoBase implements ShoppingCartDa
     }
 
     @Override
-    public ShoppingCart getByUserId(int userId) throws SQLException
-    {
+    public ShoppingCart getByUserId(int userId) {
         ShoppingCart cart = new ShoppingCart();
 
-        String query = "SELECT product_id, quantity FROM shopping_cart WHERE user_id = ?;";
+        String query = """
+                        SELECT  sc.product_id, sc.quantity,
+                                p.product_id AS p_product_id,
+                                p.name, p.price, p.category_id, p.description,
+                                p.subcategory, p.stock, p.featured, p.image_url
+                        FROM shopping_cart sc
+                        JOIN products p ON p.product_id
+                        WHERE sc.user_id = ?
+                
+                """;
 
         try (Connection connection = getConnection();
              PreparedStatement statement = connection.prepareStatement(query))
@@ -41,47 +49,52 @@ public class MySqlShoppingCartDao extends MySqlDaoBase implements ShoppingCartDa
                     cart.addItem(item);
                 }
             }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
         return cart;
     }
 
     @Override
-    public void addProduct(int userId, int productId) throws SQLException {
+    public void addProduct(int userId, int productId) {
         String query = "INSERT INTO shopping_cart (user_id, product_id, quantity) VALUES (?, ?, 1) " +
                         "ON DUPLICATE KEY UPDATE quantity = quantity + 1;";
 
         try (Connection connection = getConnection();
-             PreparedStatement statement = connection.prepareStatement(query))
-        {
+             PreparedStatement statement = connection.prepareStatement(query)) {
             statement.setInt(1, userId);
             statement.setInt(2, productId);
             statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 
     @Override
-    public void updateProductQuantity(int userId, int productId, int quantity) throws SQLException
-    {
+    public void updateProductQuantity(int userId, int productId, int quantity) {
         String query = "UPDATE shopping_cart SET quantity = ? WHERE user_id = ? AND product_id = ?;";
 
         try (Connection connection = getConnection();
-             PreparedStatement statement = connection.prepareStatement(query))
-        {
+             PreparedStatement statement = connection.prepareStatement(query)) {
             statement.setInt(1, quantity);
             statement.setInt(2, userId);
             statement.setInt(3, productId);
             statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 
     @Override
-    public void clearCart(int userId) throws SQLException{
+    public void clearCart(int userId) {
         String query = "DELETE FROM shopping_cart WHERE user_id = ?;";
 
         try (Connection connection = getConnection();
              PreparedStatement statement = connection.prepareStatement(query)) {
             statement.setInt(1, userId);
             statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 }
